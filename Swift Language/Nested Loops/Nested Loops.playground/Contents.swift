d28a2bcc137537458b1d43cfc7caffb51b2231a8
bambrmj <bambrmj@gmail.com>
2020-05-13 11:31:46 +0300

    add while, repeat & nested loops

---
+++ Swift Language/Nested Loops/Nested Loops.playground/Contents.swift
@@ -0,0 +1,34 @@
+
+// For Loop
+for i in 1...5{
+//    print(i)
+}
+
+// While Loop
+var count = 1
+while count < 6 {
+//    print(count)
+    count += 1
+}
+
+// Nested For Loop
+for i in 1...5{
+    for j in 1...10{
+//        print("i = \(i ) , j = \(j)")
+//        print("\(i) * \(j) = \(i*j)")
+    }
+}
+
+// Nested While Loop
+var i = 1
+while i < 6 {
+    var j = 1
+    while j < 11 {
+//        print("i = \(i ) , j = \(j)")
+        print("\(i) * \(j) = \(i*j)")
+        j += 1
+    }
+    i += 1
+}
+
+
---
+++ Swift Language/While & Repeat Loops/While & Repeat Loops.playground/Contents.swift
@@ -0,0 +1,17 @@
+
+
+for i in 1...5 {
+//    print(i)
+}
+
+var count = 0
+while count > 0 {
+    print(count)
+    count -= 1
+}
+
+var i = 10
+repeat {
+//    print(i)
+    i += 1
+}while i < 6
