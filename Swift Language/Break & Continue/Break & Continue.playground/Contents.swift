deac2096b72422be90933a519fc8f058f6384cd4
bambrmj <bambrmj@gmail.com>
2020-05-14 01:02:23 +0300

    update break & continue code

--- Swift Language/Break & Continue/Break & Continue.playground/Contents.swift
+++ Swift Language/Break & Continue/Break & Continue.playground/Contents.swift
@@ -1,11 +1,13 @@
 
 for i in 1...5{
     if i == 3 {
+//        break
         continue
     }
 //    print(i)
 }
 
+// Nested loop with labled statement
 var arrNumbers = [9,3,7,8,5,11]
 outerLoop: for _ in 1...5{
     for num in arrNumbers{
