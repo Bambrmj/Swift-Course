2dc03c709b8239e192bb907d5d971882cddf6553
bambrmj <bambrmj@gmail.com>
2020-05-20 01:58:55 +0300

    update closure code

--- Swift Language/Closures/Closures.playground/Contents.swift
+++ Swift Language/Closures/Closures.playground/Contents.swift
@@ -41,3 +41,46 @@ func oddNumer(num: Int) -> Bool {
 // another way to call function with closure
 let arrFiltered2 = getFilteredNumbers(closure: oddNumer(num:), arrNumbers: [2,4,5,7,8,12,13])
 print(arrFiltered2)
+
+func printSquare(closure: (Int) -> Int, arrNumbers: [Int]) {
+    for num in arrNumbers {
+        print(closure(num))
+    }
+}
+
+//printSquare(closure: { (number) -> Int in
+//    number * number
+//}, arrNumbers: [2,5,9])
+
+printSquare(closure: {$0 * $0}, arrNumbers: [3,4,6])
+
+var arrNames = ["Mohammed", "Ahmed", "ali","Majed"]
+var arrTempNames = arrNames
+//arrTempNames = arrTempNames.filter({ (name) -> Bool in
+//    name.hasPrefix("A") || name.hasPrefix("a")
+//})
+
+arrTempNames = arrTempNames.filter({$0.hasPrefix("M") || $0.hasPrefix("m")})
+print(arrTempNames)
+
+
+func printMsg(msg: String, closure: @autoclosure () -> Bool){
+    if closure() {
+        print(msg)
+    }
+}
+
+printMsg(msg: "Hello", closure: true)
+
+var arrClosures : [() -> ()] = []
+func printMsg2(msg: String, closure: @escaping () -> ()){
+    print("Hello from func \(msg)")
+    closure()
+    arrClosures.append(closure)
+}
+
+printMsg2(msg: "Call func") {
+    print("Hello from closure")
+}
+print(arrClosures)
+
