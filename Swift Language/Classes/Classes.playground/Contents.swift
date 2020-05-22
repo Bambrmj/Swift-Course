750db6cd6cb75eb961185f7af4680717f2a7e5db
bambrmj <bambrmj@gmail.com>
2020-05-22 05:52:18 +0300

    update classes code, add inheritance and polymorphism

--- Swift Language/Classes/Classes.playground/Contents.swift
+++ Swift Language/Classes/Classes.playground/Contents.swift
@@ -34,6 +34,11 @@ class Expert{
     func addNewLike(){
         likes += 1
     }
+    
+    func getName() -> String{
+        return "Expert name \(name)"
+    }
+    
 }
 // create objects from Expert class
 var expert1 = Expert(name: "Omar", bio: "IOS Developer", likes: 0)
@@ -49,3 +54,50 @@ expert1.addNewLike()
 expert1.addNewLike()
 expert1.likes
 expert2.likes
+
+
+class Developer : Expert {
+    var arrProgLanguaes : [String]
+    init(name: String, bio:String, likes: Int, arrProgLanguaes : [String]) {
+        self.arrProgLanguaes = arrProgLanguaes
+        super.init(name: name, bio: bio, likes: likes)
+    }
+    
+    override func getName() -> String {
+        return "Developer name = \(name)"
+    }
+    
+    
+}
+
+class Lawyer : Expert {
+    var numberOfCases : Int
+    init(name: String, bio:String, likes: Int, numberOfCases :Int) {
+        self.numberOfCases = numberOfCases
+        super.init(name: name, bio: bio, likes: likes)
+    }
+    
+    override func getName() -> String {
+        return "Lawyer name = \(name)"
+    }
+    
+}
+
+var developer1 = Developer(name: "Ali", bio: "IOS Developer", likes: 10, arrProgLanguaes: ["Swift","Objective-C"])
+developer1.name
+developer1.addNewLike()
+developer1.likes
+var lawyer1 = Lawyer(name: "Salem", bio: "", likes: 8, numberOfCases: 12)
+lawyer1.likes
+//print(developer1.getName())
+//print(lawyer1.getName())
+
+var arrExpers : [Expert] = [developer1, lawyer1]
+for expert in arrExpers{
+    print(expert.getName())
+    if let developer = expert as? Developer{
+        print(developer.arrProgLanguaes)
+    }else if let lawyer = expert as? Lawyer {
+        print(lawyer.numberOfCases)
+    }
+}
